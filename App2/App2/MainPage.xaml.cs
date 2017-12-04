6290105ff64dcf93f6bc09ca024e53372ea1acaa
Julien Rosen <jlrosen416@gmail.com>
2017-12-04 15:15:55 -0500

    Add project files.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using PropertyChanged;
+using System.Diagnostics;
+
+namespace App2
+{
+    [AddINotifyPropertyChangedInterface]
+    public partial class MainPage : ContentPage
+    {
+        public bool CurrentState { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        private void OnCurrentStateChanged()
+        {
+            Debugger.Break();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            CurrentState = !CurrentState;
+        }
+    }
+}
